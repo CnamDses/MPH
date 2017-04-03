13461f865b18d010e1a96b161db8dd89e2344580
BEN SASSI FIRAS <firas.fbs.1989@gmail.com>
2017-04-03 17:43:06 +0200

    some changes

--- Main.py
+++ Main.py
@@ -8,19 +8,26 @@ import matplotlib.pyplot as plt
 import seaborn as sns # pip install seaborn
 
 
+
+def get_data(SAS):
+	if SAS:
+    	data = pd.read_sas('/data.SAS7bDAT', encoding='utf-8', format=True, sep=';')
+	else:
+		data = pd.read_csv('D://mnist/train.csv', encoding='utf-8', sep=';')
+	return data
+
+
+def extract_test_train_datasets(data):
+	df = pd.DataFrame(np.random.randn(4000000, 2))
+	msk = np.random.rand(len(df)) < 0.8
+	train = df[msk]
+	test = df[~msk]
+	return (train, test)
+
 # Import des données
-bool SAS = True
-if SAS:
-    data = pd.read_sas('../data/data.SAS7bDAT', encoding='utf-8', format=True, sep=';')
-else:
-	data = pd.read_csv('../data/train.csv', encoding='utf-8', sep=';')
-
+data = get_data(False)
 # Diviser les données en échantilons d'apprentissage et de tests
-df = pd.DataFrame(np.random.randn(4000000, 2))
-msk = np.random.rand(len(df)) < 0.8
-train = df[msk]
-test = df[~msk]
-
+(train,test) = extract_test_train_datasets(data)
 #Analyse descriptive
 
 #Préparation des données
@@ -47,27 +54,50 @@ train['Y'] = train['ColonneApredire'] #changement de la colonne a predire
 def score(y_true, y_pred): 
     return np.mean(np.abs(y_true - y_pred)) * 100
 	
-	
-#Cross validation pour le calibrage du modéle Random Forest
-err = 0
-NBROUND = 5 #nomnbre de chunks
-FEATURES = feat_num+feat_cat # on n'utilise que ces features pour ce modèle basique
-for train_index, test_index in KFold(train.shape[0], n_folds=NBROUND):
-    y = train['Y']
-    X = train[FEATURES]
-    X_train, X_test = X.ix[train_index, :], X.ix[test_index, :]
-    y_train, y_test = y[train_index], y[test_index]
-    clf = RandomForestRegressor()
-    clf.fit(X_train, y_train)
-    pred = clf.predict(X_test)
-    
-    err += score(y_test, pred)
-    print score(y_test, pred)
-print "*** accuracy_score : ", err / NBROUND
+# Cross validation pour le calibrage du modéle de prédiction
+model = "RandomForest"
+if(model == "RandomForest"):
+	err = 0
+	NBROUND = 5 #nomnbre de chunks
+	FEATURES = feat_num+feat_cat # on n'utilise que ces features pour ce modèle basique
+	for train_index, test_index in KFold(train.shape[0], n_folds=NBROUND):
+	    y = train['Y']
+	    X = train[FEATURES]
+	    X_train, X_test = X.ix[train_index, :], X.ix[test_index, :]
+	    y_train, y_test = y[train_index], y[test_index]
+	    clf = RandomForestRegressor()
+	    clf.fit(X_train, y_train)
+	    pred = clf.predict(X_test)
+	    
+	    err += score(y_test, pred)
+	    print score(y_test, pred)
+	print "*** accuracy_score RandomForest: ", err / NBROUND
+	#Calcul des prédictions sur l'échantillon de test
+	clf = RandomForestRegressor()
+	# On entraine de nouveau le modèle, cette fois sur l'intégralité des données
+	clf.fit(train[FEATURES], train['Y'])
+	predictions = np.exp(clf.predict(test[FEATURES]))
+else if (model == "LogisticRegression"):
+	from sklearn import linear_model
+	err = 0
+	NBROUND = 5 #nomnbre de chunks
+	FEATURES = feat_num+feat_cat # on n'utilise que ces features pour ce modèle basique
+	for train_index, test_index in KFold(train.shape[0], n_folds=NBROUND):
+	    y = train['Y']
+	    X = train[FEATURES]
+	    X_train, X_test = X.ix[train_index, :], X.ix[test_index, :]
+	    y_train, y_test = y[train_index], y[test_index]
+	    clf = linear_model.LogisticRegression()
+	    clf.fit(X_train, y_train)
+	    pred = clf.predict(X_test)
+	    
+	    err += score(y_test, pred)
+	    print score(y_test, pred)
+	print "*** accuracy_score Logistic Regression : ", err / NBROUND
+	#Calcul des prédictions sur l'échantillon de test
+	clf = RandomForestRegressor()
+	# On entraine de nouveau le modèle, cette fois sur l'intégralité des données
+	clf.fit(train[FEATURES], train['Y'])
+	predictions = np.exp(clf.predict(test[FEATURES]))
 
 
-#Calcul des prédictions sur l'échantillon de test
-clf = RandomForestRegressor()
-# On entraine de nouveau le modèle, cette fois sur l'intégralité des données
-clf.fit(train[FEATURES], train['Y'])
-predictions = np.exp(clf.predict(test[FEATURES]))
