b8e9e767402e81bf3fc7676cb84229ceb58e9bbf
Unknown <firas.fbs.1989@gmail.com>
2017-04-10 12:11:50 +0200

    MPH 3 : refactoring du code
    rendre le code plus modulable

--- Main.py
+++ Main.py
@@ -4,11 +4,10 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.cross_validation import KFold
 from sklearn.ensemble import RandomForestRegressor
 import matplotlib.pyplot as plt
-%matplotlib inline 
 import seaborn as sns # pip install seaborn
 
 
-
+#charegement des donénes
 def get_data(SAS):
 	if SAS:
     	data = pd.read_sas('/data.SAS7bDAT', encoding='utf-8', format=True, sep=';')
@@ -16,7 +15,7 @@ def get_data(SAS):
 		data = pd.read_csv('D://mnist/train.csv', encoding='utf-8', sep=';')
 	return data
 
-
+#extraction échantillon d'apprentissage et de test
 def extract_test_train_datasets(data):
 	df = pd.DataFrame(np.random.randn(4000000, 2))
 	msk = np.random.rand(len(df)) < 0.8
@@ -28,15 +27,37 @@ def extract_test_train_datasets(data):
 def score(y_true, y_pred): 
     return np.mean(np.abs(y_true - y_pred)) * 100
 
-# Import des données
+#fitting model
+def fit_model(model, NBROUND, FEATURES, train, Y):
+	err = 0
+	if(model = "RF"):
+		clf = RandomForestRegressor()
+		else clf = linear_model.LogisticRegression()
+
+	for train_index, test_index in KFold(train.shape[0], n_folds=NBROUND):
+		y = train[Y]
+	    X = train[FEATURES]
+	    X_train, X_test = X.ix[train_index, :], X.ix[test_index, :]
+	    y_train, y_test = y[train_index], y[test_index]
+			   
+	    clf.fit(X_train, y_train)
+	    pred = clf.predict(X_test)
+			    
+	    err += score(y_test, pred)
+	    print score(y_test, pred)
+		print "*** accuracy_score RandomForest: ", err / NBROUND
+	return clf
+			
+
+#Le script 
+
 data = get_data(False)
-# Diviser les données en échantilons d'apprentissage et de tests
+
 (train,test) = extract_test_train_datasets(data)
-#Analyse descriptive
 
 #Préparation des données
 # features numériques
-feat_num = ['nom de colonne']
+feat_num = ['nom de colonne1', 'nom de colonne2']
 # features date
 feat_dates = []
 # features catégorielles
@@ -51,55 +72,8 @@ for c in feat_cat:
     train[c] = le.transform(train[c])
     test[c] = le.transform(test[c])
 	
-#Création du modéle
-train['Y'] = train['ColonneApredire'] #changement de la colonne a predire
+#Construction du modéle de prédiction
+fit_model("RF", 5, feat_cat + feat_num, train, "NomColonneApredire")
 
-
-	
-# Cross validation pour le calibrage du modéle de prédiction
-model = "RandomForest"
-if(model == "RandomForest"):
-	err = 0
-	NBROUND = 5 #nomnbre de chunks
-	FEATURES = feat_num+feat_cat # on n'utilise que ces features pour ce modèle basique
-	for train_index, test_index in KFold(train.shape[0], n_folds=NBROUND):
-	    y = train['Y']
-	    X = train[FEATURES]
-	    X_train, X_test = X.ix[train_index, :], X.ix[test_index, :]
-	    y_train, y_test = y[train_index], y[test_index]
-	    clf = RandomForestRegressor()
-	    clf.fit(X_train, y_train)
-	    pred = clf.predict(X_test)
-	    
-	    err += score(y_test, pred)
-	    print score(y_test, pred)
-	print "*** accuracy_score RandomForest: ", err / NBROUND
-	#Calcul des prédictions sur l'échantillon de test
-	clf = RandomForestRegressor()
-	# On entraine de nouveau le modèle, cette fois sur l'intégralité des données
-	clf.fit(train[FEATURES], train['Y'])
-	predictions = np.exp(clf.predict(test[FEATURES]))
-else if (model == "LogisticRegression"):
-	from sklearn import linear_model
-	err = 0
-	NBROUND = 5 #nomnbre de chunks
-	FEATURES = feat_num+feat_cat # on n'utilise que ces features pour ce modèle basique
-	for train_index, test_index in KFold(train.shape[0], n_folds=NBROUND):
-	    y = train['Y']
-	    X = train[FEATURES]
-	    X_train, X_test = X.ix[train_index, :], X.ix[test_index, :]
-	    y_train, y_test = y[train_index], y[test_index]
-	    clf = linear_model.LogisticRegression()
-	    clf.fit(X_train, y_train)
-	    pred = clf.predict(X_test)
-	    
-	    err += score(y_test, pred)
-	    print score(y_test, pred)
-	print "*** accuracy_score Logistic Regression : ", err / NBROUND
-	#Calcul des prédictions sur l'échantillon de test
-	clf = RandomForestRegressor()
-	# On entraine de nouveau le modèle, cette fois sur l'intégralité des données
-	clf.fit(train[FEATURES], train['Y'])
-	predictions = np.exp(clf.predict(test[FEATURES]))
 
 
